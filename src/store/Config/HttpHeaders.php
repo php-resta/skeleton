095ddf6c34d7be530d31be56010a40448b424772
aligurbuz <ali.gurbuz@filonclub.com>
2017-11-28 14:25:59 +0000

    resta structured

---
+++ src/store/Config/HttpHeaders.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Store\Config;
+
+class HttpHeaders {
+
+    public static $httpHeaders=[
+
+        'host',
+        'connection',
+        'cache-control',
+        'accept',
+        'upgrade-insecure-requests',
+        'user-agent',
+        'accept-encoding',
+        'accept-language',
+        'cookie',
+        'postman-token',
+        'content-length',
+        'origin',
+        'content-type',
+        'clientToken'
+    ];
+}
