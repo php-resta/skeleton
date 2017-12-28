bdbeeb083ce8ffafaf59c895bb06c15b04f0198c
aligurbuz <galiant781@gmail.com>
2017-12-28 19:26:49 +0000

    collection class

---
+++ src/store/Services/AppCollection.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Store\Services;
+
+use Illuminate\Support\Collection;
+
+class AppCollection {
+
+    /**
+     * @param array $data
+     * @return Collection
+     */
+    public function collect($data=array()){
+
+        return collect($data);
+    }
+
+}
