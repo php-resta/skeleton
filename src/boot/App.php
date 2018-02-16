6a82f77c68460a063e5ea876d644e9422cf8c0a7
aligurbuz <galiant781@gmail.com>
2018-02-16 19:40:50 +0000

    app boot

---
+++ src/boot/App.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Boot;
+
+use Resta\ApplicationProvider;
+
+class App extends ApplicationProvider  {
+
+    /**
+     * @method boot
+     */
+    public function boot(){
+
+        //do somethings
+    }
+
+}
