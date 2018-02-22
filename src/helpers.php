82a169f54f37ef978afbaf819546c44b20e233e1
aligurbuz <galiant781@gmail.com>
2018-02-22 19:17:46 +0000

    helpers

--- src/helpers.php
+++ src/helpers.php
@@ -28,3 +28,41 @@ if (!function_exists('app')) {
         return \application::getAppInstance();
     }
 }
+
+if (!function_exists('request')) {
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Request
+     */
+    function request()
+    {
+        return app()->request();
+    }
+}
+
+
+if (!function_exists('post')) {
+
+    /**
+     * @param null $param
+     * @return mixed
+     */
+    function post($param=null)
+    {
+        return app()->post($param);
+    }
+}
+
+
+
+if (!function_exists('get')) {
+
+    /**
+     * @param null $param
+     * @return mixed
+     */
+    function get($param=null)
+    {
+        return app()->get($param);
+    }
+}
