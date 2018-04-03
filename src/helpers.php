72f4896b0abf787c37a053188123b786c6c0ccd5
agurbuz <agurbuz@tatilemerhaba.com>
2018-04-03 16:45:37 +0300

    helpers

--- src/helpers.php
+++ src/helpers.php
@@ -32,7 +32,7 @@ if (!function_exists('appInstance')) {
 if (!function_exists('app')) {
 
     /**
-     * @return \Resta\Contracts\ApplicationContracts
+     * @return \Resta\Contracts\ApplicationContracts|\Resta\Contracts\ApplicationPathContracts
      */
     function app()
     {
