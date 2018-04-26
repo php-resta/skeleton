63fc6e265d5fe57f183f09b845cc96bf91a0ada9
agurbuz <agurbuz@tatilemerhaba.com>
2018-04-26 15:02:35 +0300

    appKernel changed

--- src/app/appKernel.php
+++ src/app/appKernel.php
@@ -2,47 +2,15 @@
 
 namespace App;
 
-use Boot\Encrypter;
-use Resta\Booting\ApplicationInstanceLoader;
-use Resta\Booting\ConfigLoader;
-use Resta\Booting\Environment;
-use Resta\Booting\Exception;
-use Resta\Booting\GlobalAccessor;
-use Resta\Booting\Console;
-use Resta\Booting\LogProvider;
-use Resta\Booting\Middleware;
-use Resta\Booting\ResponseManager;
-use Resta\Booting\RouteProvider;
-use Resta\Booting\ServiceContainer;
-use Resta\Booting\UrlParse;
+use Resta\Foundation\Kernel;
 
-class appKernel {
+class appKernel extends Kernel {
 
     /**
-     * @var array
+     * Here you can take over the middlewareGroups and bootstrappers properties
+     * in the kernel and have the freedom to change them as you like.
+     * !!! Be careful that any bugs can destroy your system.
+     * !!! Do not touch here unless you're very good.
      */
-    protected $middlewareGroups=[
 
-        ApplicationInstanceLoader::class,
-        GlobalAccessor::class,
-        Exception::class,
-        UrlParse::class,
-        LogProvider::class,
-        Environment::class,
-        Encrypter::class,
-        ConfigLoader::class,
-        ServiceContainer::class,
-        Middleware::class
-    ];
-
-    /**
-     * @var array
-     */
-    protected $bootstrappers=[
-
-        Console::class,
-        RouteProvider::class,
-        ResponseManager::class,
-
-    ];
 }
