967c85831a8f2a2c118936dba88d26cf2e3795e5
agurbuz <agurbuz@tatilemerhaba.com>
2018-05-07 16:42:41 +0300

    bootstraper directory

---
+++ bootstrapper/applicationStart.php
@@ -0,0 +1,32 @@
+<?php
+
+error_reporting(0);
+
+//micro time starter for resta response time
+define("time_start",microtime(true));
+
+//root definer: application root path
+define("root",str_replace("\\","/",realpath(__DIR__.'/../')));
+
+/**
+ * resta sysmte composer vendor autoload.
+ * For libraries that specify autoload information, Composer generates a vendor/autoload.php file.
+ * You can simply include this file and start using the classes that those libraries provide without any extra work
+ * system main skeleton
+ * return autoload file
+ */
+require_once root.'/vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Application Starting
+|--------------------------------------------------------------------------
+|
+| This class is the starter of your application. This class is used when the
+| resta firstly calls.all request coming to application are run here.
+| resta starter place
+|
+*/
+
+//load spl autoload register
+(new \Resta\autoloadRegister())->register();
