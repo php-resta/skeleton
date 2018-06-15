7dca11b8dd5cd059d7dbfdc8c54516949e3d63bf
aligurbuz <galiant781@gmail.com>
2018-06-15 06:03:48 +0000

    boot interface for application boot

--- src/boot/App.php
+++ src/boot/App.php
@@ -4,8 +4,9 @@ namespace Boot;
 
 use Store\Traits\Annotations;
 use Resta\ApplicationProvider;
+use Resta\Contracts\BootContracts;
 
-class App extends ApplicationProvider  {
+class App extends ApplicationProvider implements BootContracts {
 
     use Annotations;
 
