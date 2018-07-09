e6cbbe91dda03e3891d41d1c0d5ba60454d16a04
agurbuz <agurbuz@tatilemerhaba.com>
2018-07-09 11:57:36 +0300

    manifest has been moved

---
+++ bootstrapper/Manifest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Bootstrapper;
+
+use Resta\Foundation\Kernel;
+
+class Manifest extends Kernel {
+
+    /**
+     * Here you can take over the middlewareGroups and bootstrappers properties
+     * in the kernel and have the freedom to change them as you like.
+     * !!! Be careful that any bugs can destroy your system.
+     * !!! Do not touch here unless you're very good.
+     */
+
+
+}
