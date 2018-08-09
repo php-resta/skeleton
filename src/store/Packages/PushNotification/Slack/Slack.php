2bafecaf652a89e76a5791befe59d1282ed42a1d
agurbuz <agurbuz@tatilemerhaba.com>
2018-08-09 16:00:00 +0300

    package slack push notification

---
+++ src/store/Packages/PushNotification/Slack/Slack.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Store\Packages\PushNotification\Slack;
+
+class Slack {
+
+    /**
+     * @var $hook
+     */
+    protected static $hook;
+
+    /**
+     * @var $channel
+     */
+    protected static $channel;
+
+    /**
+     * @param string $channel
+     * @return Slack
+     */
+    public static function channel($channel='default'){
+
+        //we get configuration settings for slack.
+        $config=config('slack.'.$channel);
+
+        //channel and hook information.
+        self::$channel=$config['channel'];
+        self::$hook=$config['hook'];
+
+        //return static object
+        return new static();
+    }
+
+    /**
+     * @param $message
+     * @return mixed
+     */
+    public static function push($message){
+
+        // if the hook object has a null value,
+        // we will set the default value to channel.
+        if(self::$hook===null){
+            self::channel('default');
+        }
+
+        //as a result we send push notifications for slack.
+        return (new SlackConnection(self::$hook))->handle(self::$channel,$message);
+    }
+}
---
+++ src/store/Packages/PushNotification/Slack/SlackConnection.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Store\Packages\PushNotification\Slack;
+
+class SlackConnection {
+
+    /**
+     * @var $hook
+     */
+    protected $hook;
+
+    /**
+     * SlackConnection constructor.
+     * @param $hook
+     */
+    public function __construct($hook) {
+        $this->hook=$hook;
+    }
+
+    /**
+     * @param $channel
+     * @param $message
+     * @return bool
+     */
+    public function handle($channel,$message){
+
+        // for push notification
+        // we have to specify the payload value.
+        $data = "payload=" . json_encode(array(
+                "channel"       =>  "#".$channel,
+                "text"          =>  $message,
+            ));
+
+        // We are starting curl for the hook value.
+        $ch = curl_init($this->hook);
+
+        // We are entering our curl option values ​​for push notification.
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        //finally,return result
+        return ($result=="ok") ? true : false;
+    }
+}
