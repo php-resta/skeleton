b5920e3cfbf31a7df9e9585802892bccb90d5051
aligurbuz <galiant781@gmail.com>
2018-09-01 11:06:24 +0000

    composer.lock

--- tests/Unit/exampleTest.php
+++ tests/Unit/exampleTest.php
@@ -12,6 +12,26 @@ class exampleTest extends TestCase
      */
     public function testFooBar()
     {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testFooBar2()
+    {
         $this->assertTrue(false);
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testFooBar3()
+    {
+        $this->assertTrue(true);
     }
 }
