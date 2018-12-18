41c7556fa815b977d9d981cced5f06b05de1c530
aligurbuz <galiant781@gmail.com>
2018-12-18 15:18:29 +0000

    manifest

--- bootstrapper/Manifest.php
+++ bootstrapper/Manifest.php
@@ -3,11 +3,25 @@
 namespace Bootstrapper;
 
 use Resta\Foundation\Kernel;
+use Resta\Contracts\BootContracts;
 
-class Manifest extends Kernel
+class Manifest extends Kernel implements BootContracts
 {
     /**
      * @var array $revision
      */
-    protected static $revision = [];
+    protected $revision = [];
+
+    /**
+     * @var array
+     */
+    protected $afterOriginGroups = [];
+
+    /**
+     * @return array
+     */
+    public function boot() : array
+    {
+        return $this->afterOriginGroups;
+    }
 }
