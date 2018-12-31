0d3156a74f1863e63bcaa004583738ffb77e7cb8
aligurbuz <galiant781@gmail.com>
2018-12-31 09:23:20 +0300

    illuminate paginator current resolve boot

--- src/boot/App.php
+++ src/boot/App.php
@@ -5,6 +5,7 @@ namespace Boot;
 use Store\Traits\Annotations;
 use Resta\ApplicationProvider;
 use Resta\Contracts\BootContracts;
+use Illuminate\Pagination\Paginator;
 use Store\Packages\Database\Eloquent\Connection as Eloquent;
 
 class App extends ApplicationProvider implements BootContracts
@@ -18,6 +19,10 @@ class App extends ApplicationProvider implements BootContracts
     {
         //eloquent connection
         new Eloquent();
+
+        Paginator::currentPageResolver(function(){
+            return get('page',1);
+        });
     }
 
 }
