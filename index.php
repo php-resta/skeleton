ad1ae59cd5b31046698b55b39601b4257b5cc02d
aligurbuz <galiant781@gmail.com>
2019-01-15 13:51:30 +0300

    index.php typo fix

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
 <?php
 
 /**
- * resta system composer vendor autoload.
+ * Resta core system composer vendor autoload.
  * For libraries that specify autoload information, Composer generates a vendor/autoload.php file.
  * You can simply include this file and start using the classes that those libraries provide without any extra work
- * system main skeleton
- * return autoload file
  */
 require_once 'bootstrapper'.DIRECTORY_SEPARATOR.'ApplicationStart.php';
 
 use Resta\Support\Utils;
 
-$app=new Resta\Foundation\Application(Utils::isRequestConsole());
+$app = new Resta\Foundation\Application(Utils::isRequestConsole());
 echo $app->handle();
 
