9c9a4a8f5bfea00fa13e377afbd846780f77a2af
aligurbuz <galiant781@gmail.com>
2019-01-26 10:23:29 +0000

    manifest bootstrap.php

--- bootstrapper/ApplicationStart.php
+++ bootstrapper/ApplicationStart.php
@@ -3,17 +3,14 @@
 //micro time starter for resta response time
 define("time_start",microtime(true));
 
-//root definer: application root path
-define("root",str_replace("\\","/",realpath(__DIR__.'/../')));
-
 /**
- * resta system composer vendor autoload.
+ * The resta system composer vendor autoload.
  * For libraries that specify autoload information, Composer generates a vendor/autoload.php file.
  * You can simply include this file and start using the classes that those libraries provide without any extra work
  * system main skeleton
  * return autoload file
  */
-require_once root.''.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
+require_once 'Bootstrap.php';
 
 /*
 |--------------------------------------------------------------------------
---
+++ bootstrapper/Bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+//root definer: application root path
+define("root",str_replace("\\","/",realpath(__DIR__.'/../')));
+
+/**
+ * The resta system composer vendor autoload.
+ * For libraries that specify autoload information, Composer generates a vendor/autoload.php file.
+ * You can simply include this file and start using the classes that those libraries provide without any extra work
+ * system main skeleton
+ * return autoload file
+ */
+require_once root.''.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
