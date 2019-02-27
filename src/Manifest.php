a2b43c0cdb8e93930a6f27b60c87445a7bc37263
aligurbuz <galiant781@gmail.com>
2019-02-27 19:46:26 +0000

    manifest property developing

--- src/Manifest.php
+++ src/Manifest.php
@@ -7,7 +7,30 @@ use Resta\Foundation\Kernel;
 class Manifest extends Kernel
 {
     /**
+     * Changes any kernel group objects.
+     *
      * @var array $revision
      */
     protected $revision = [];
+
+    /**
+     * this property changes the kernel life cycle.
+     *
+     * @var array $app
+     */
+    protected $app = [
+
+        /*
+        |--------------------------------------------------------------------------
+        | Kernel Service Providers
+        |--------------------------------------------------------------------------
+        |
+        | The kernel service providers listed here will be automatically loaded on the
+        | request to your system. Feel free to add your own services to
+        | this array to grant expanded functionality to your applications.
+        |
+        */
+        'providers' => []
+
+    ];
 }
