4f802b2351388320bb17d01d26f502039b8b9bd7
aligurbuz <galiant781@gmail.com>
2019-03-29 14:52:49 +0000

    response fix

--- src/store/Config/Response.php
+++ src/store/Config/Response.php
@@ -4,6 +4,18 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Application Response Configuration
+    |--------------------------------------------------------------------------
+    |
+    | The application response determines the default output type that will be used
+    | by the result service endpoint. You are free to set this value
+    | to any of the response which will be supported by the application.
+    |
+    */
+    'response' => 'json',
+
+    /*
+    |--------------------------------------------------------------------------
     | Application Response Meta
     |--------------------------------------------------------------------------
     |
@@ -29,7 +41,21 @@ return [
      | the same keys over and over to the file that will create your documentation.
      |
      */
-    'data' => 'resource'
+    'data' => 'resource',
+
+    /*
+   |--------------------------------------------------------------------------
+   | Application Response OutPutter
+   |--------------------------------------------------------------------------
+   |
+   | you can specify and manage the response output according to your project group.
+   | This value in the config is defined by the kernel and
+   | is a config switch which should never be disabled.
+   |
+   */
+    'outputter' => [
+        'formatter'  => 'Store\Services\Response'
+    ],
 
 
 ];
---
+++ src/store/Services/Response.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Store\Services;
+
+use Spatie\ArrayToXml\ArrayToXml;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
+
+class Response
+{
+    /**
+     * json response formatter
+     *
+     * @param $outputter
+     * @return mixed
+     */
+    public function json($outputter){
+
+        //header set and symfony response call
+        header('Content-type:application/json;charset=utf-8');
+        $response = new HttpResponse();
+
+        //json data set and get content from symfony response
+        $response->setContent(json_encode($outputter));
+        return $response->getContent();
+    }
+
+    /**
+     * xml response formatter
+     *
+     * @param $outputter
+     * @return string
+     */
+    public function xml($outputter){
+
+        header('Content-type:application/xml;charset=utf-8');
+        return ArrayToXml::convert($outputter);
+    }
+}
