9645fc23936cb7bdffeadf19a6c826bd86540046
aligurbuz <galiant781@gmail.com>
2019-04-28 09:43:59 +0000

    skeleton directory has been updated docker and public directory

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
  * For libraries that specify autoload information, Composer generates a vendor/autoload.php file.
  * You can simply include this file and start using the classes that those libraries provide without any extra work
  */
-require_once 'bootstrapper'.DIRECTORY_SEPARATOR.'ApplicationStart.php';
+require_once '../bootstrapper'.DIRECTORY_SEPARATOR.'ApplicationStart.php';
 
 use Resta\Foundation\Application;
 
