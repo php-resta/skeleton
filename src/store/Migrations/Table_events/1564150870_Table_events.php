011edb4b0e876e0f537f714b63bd03368a1d01bd
aligurbuz <galiant781@gmail.com>
2019-07-26 14:22:57 +0000

    table events migration

---
+++ src/store/Migrations/Table_events/1564150870_Table_events.php
@@ -0,0 +1,43 @@
+<?php
+
+use Migratio\Contract\MigrationContract;
+use Migratio\Contract\WizardContract as Wizard;
+use Migratio\Contract\SchemaCapsuleContract as Schema;
+
+class Table_events implements MigrationContract
+{
+    /**
+     * Run the migrations.
+     *
+     * @param Schema $schema
+     * @return mixed
+     */
+    public function up(Schema $schema)
+    {
+        return $schema->create(function(Wizard $wizard){
+
+            $wizard->name('id')->int(14)->auto_increment();
+            $wizard->name('table_name')->varchar(255)->index();
+            $wizard->name('table_field')->varchar(255)->index();
+            $wizard->name('old_value')->longtext();
+            $wizard->name('new_value')->longtext();
+            $wizard->name('event_name')->varchar(255)->index();
+            $wizard->name('client_ip')->bigint(20)->index();
+            $wizard->name('auth_id')->int(11)->index();
+            $wizard->name('created_at')->datetime();
+            $wizard->table()->collation('utf8');
+        
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
