34b085278b723aae61553522d9842e2c48a3b1e8
aligurbuz <galiant781@gmail.com>
2019-08-10 15:58:16 +0000

    role create state

--- src/store/Migrations/Roles/RoleCreate.php
+++ src/store/Migrations/Roles/RoleCreate.php
@@ -20,6 +20,7 @@ class RoleCreate implements MigrationContract
             $wizard->name('name')->varchar(255)->index();
             $wizard->name('status')->int()->default(1)->index();
             $wizard->name('is_deleted')->int()->default(0)->index();
+            $wizard->name('role_state')->varchar(255)->index();
 
             $wizard->name('created_at')->dateTime();
             $wizard->name('updated_at')->dateTime();
