af2efe4e82470f4071403356a53e782657723f44
aligurbuz <galiant781@gmail.com>
2019-08-19 07:42:09 +0000

    store migration fix

--- src/store/Migrations/Migrations/1566198437_MigrationCreate.php
+++ src/store/Migrations/Migrations/1566198437_MigrationCreate.php
@@ -4,7 +4,7 @@ use Migratio\Contract\MigrationContract;
 use Migratio\Contract\WizardContract as Wizard;
 use Migratio\Contract\SchemaCapsuleContract as Schema;
 
-class PermissionCreate implements MigrationContract
+class MigrationCreate implements MigrationContract
 {
     /**
      * Run the migrations.
@@ -17,12 +17,10 @@ class PermissionCreate implements MigrationContract
         return $schema->create(function(Wizard $wizard){
 
             $wizard->auto_increment();
-            $wizard->name('name_code')->int()->index();
-            $wizard->name('route_name')->varchar(255)->index();
-            $wizard->name('role_id')->int()->index();
+            $wizard->name('name')->text()->index();
 
-            $wizard->name('created_at')->dateTime();
-            $wizard->name('updated_at')->dateTime();
+            $wizard->name('created_at')->timestamp();
+            $wizard->name('updated_at')->timestamp();
 
         });
     }
