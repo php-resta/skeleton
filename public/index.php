163829e5be203a31c6faea1e19c1ddc9dede4c8e
aligurbuz <galiant781@gmail.com>
2019-10-06 11:18:04 +0000

    index.php fix

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, PUT,DELETE,OPTIONS,HEAD');
 header('Access-Control-Allow-Headers: token,apikey,Content-Type');
 
 /**
