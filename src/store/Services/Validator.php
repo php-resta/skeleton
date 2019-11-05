a27bf0689ff3abb7a412aa095315c12a689856cc
aligurbuz <galiant781@gmail.com>
2019-11-05 11:07:03 +0000

    composer.json fix and validator formatting

--- src/store/Services/Validator.php
+++ src/store/Services/Validator.php
@@ -2,17 +2,22 @@
 
 namespace Store\Services;
 
+use Resta\Contracts\ClientContract;
 use Zend\Validator\Date;
 use Zend\Validator\CreditCard;
 use Zend\Validator\EmailAddress;
+use libphonenumber\PhoneNumberUtil;
+use libphonenumber\NumberParseException;
 
 class Validator
 {
     /**
      * @param $email
+     * @param ClientContract $client
+     * @param null|string $input
      * @return bool
      */
-    public static function email($email)
+    public static function email($email,ClientContract $client,$input=null)
     {
         //is array control for email
         $email=self::checkArrayValidator($email);
@@ -33,9 +38,11 @@ class Validator
 
     /**
      * @param $creditCardNo
+     * @param ClientContract $client
+     * @param null|string $input
      * @return bool
      */
-    public static function creditCard($creditCardNo)
+    public static function creditCard($creditCardNo,ClientContract $client,$input=null)
     {
         //is array control for credit card
         $creditCardNo=self::checkArrayValidator($creditCardNo);
@@ -55,11 +62,14 @@ class Validator
 
     /**
      * @param $date
-     * @param array $format
+     * @param ClientContract $client
+     * @param null|string $input
      * @return bool
      */
-    public static function date($date,$format=['format'=>'Y-m-d'])
+    public static function date($date,ClientContract $client,$input=null)
     {
+        $format=['format'=>'Y-m-d'];
+
         //is array control for credit card
         $date=self::checkArrayValidator($date);
 
@@ -74,6 +84,51 @@ class Validator
             }
         }
         return true;
+    }
+
+    /**
+     * check phone number from request input
+     *
+     * @param null|string $phone
+     * @param ClientContract $client
+     * @param null|string $input
+     * @return mixed|void
+     */
+    public static function phoneNumber($phone,ClientContract $client=null,$input=null)
+    {
+        $phoneUtil = PhoneNumberUtil::getInstance();
+
+        if($client->has('phone_code')){
+            $countryCode = $phoneUtil->getRegionCodeForCountryCode(
+                str_replace('+','',$client->input('phone_code'))
+            );
+        }
+        else{
+            $geo = self::geoPluginInstance();
+            $countryCode = $geo['geoplugin_countryCode'];
+        }
+
+        try {
+            $swissNumberProto = $phoneUtil->parse($phone, $countryCode);
+
+            $valid = $phoneUtil->isValidNumber($swissNumberProto);
+
+            if(false === $valid){
+                exception('phoneNumber',['key'=>$input.':'.$phone])->invalidArgument('phone number is not valid');
+            }
+
+            if(app()->has('phoneUtil')){
+                app()->terminate('phoneUtil');
+            }
+
+            app()->register('phoneUtil',$swissNumberProto);
+
+        } catch (NumberParseException $e) {
+            if(!is_numeric($countryCode)){
+                exception('phoneNumber',['key'=>'phone_code'.':'.$client->input('phone_code')])->invalidArgument($e->getMessage());
+            }
+            exception('phoneNumber',['key'=>$input.':'.$phone])->invalidArgument($e->getMessage());
+        }
     }
 
     /**
@@ -97,5 +152,19 @@ class Validator
     private static function checkArrayValidator($data)
     {
         return (is_array($data)) ? $data : [$data];
+    }
+
+    /**
+     * get geo plugin instance
+     *
+     * @return mixed
+     */
+    private static function geoPluginInstance()
+    {
+        if(false === app()->has('geoPluginInstance')){
+            app()->register('geoPluginInstance',json_decode(file_get_contents('http://www.geoplugin.net/json.gp'),1));
+        }
+
+        return app()->get('geoPluginInstance');
     }
 }
