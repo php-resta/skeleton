c060b2f86a3c39e325645c6a3138aa2f426e7100
aligurbuz <galiant781@gmail.com>
2019-11-26 10:47:42 +0000

    store fix

--- src/store/Packages/Database/Eloquent/Connection.php
+++ src/store/Packages/Database/Eloquent/Connection.php
@@ -32,6 +32,15 @@ class Connection
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
+            'strict'    => true,
+            'modes'  => [
+                //'ONLY_FULL_GROUP_BY',
+                'STRICT_TRANS_TABLES',
+                'NO_ZERO_IN_DATE',
+                'NO_ZERO_DATE',
+                'ERROR_FOR_DIVISION_BY_ZERO',
+                'NO_ENGINE_SUBSTITUTION',
+            ]
         ]);
 
         // Make this Capsule instance available globally via static methods... (optional)
