75016ae01acb04fe300850b13af78eb5993833ca
aligurbuz <ali@munch.com.tr>
2020-11-26 09:25:22 +0000

    packages database eloquent connection fix

--- src/store/Packages/Database/Eloquent/Connection.php
+++ src/store/Packages/Database/Eloquent/Connection.php
@@ -23,25 +23,78 @@ class Connection
 
         $configdb = DatabaseConnection::getConfig();
 
-        $this->capsule->addConnection([
-            'driver'    => (isset($configdb['driver'])) ? $configdb['driver'] : 'mysql' ,
-            'host'      => (isset($configdb['host'])) ? $configdb['host'] : '127.0.0.1',
-            'database'  => (isset($configdb['database'])) ? $configdb['database'] : 'test',
-            'username'  => (isset($configdb['user'])) ? $configdb['user'] : 'root',
-            'password'  => (isset($configdb['password'])) ? $configdb['password'] : 'root',
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-            'strict'    => true,
-            'modes'  => [
-                //'ONLY_FULL_GROUP_BY',
-                'STRICT_TRANS_TABLES',
-                'NO_ZERO_IN_DATE',
-                'NO_ZERO_DATE',
-                'ERROR_FOR_DIVISION_BY_ZERO',
-                'NO_ENGINE_SUBSTITUTION',
-            ]
-        ]);
+        if(isset($configdb['read'],$configdb['write'])){
+
+            $readConfig = $configdb['read'];
+            $writeConfig = $configdb['write'];
+
+
+            $this->capsule->addConnection([
+                'read' => [
+                    'driver'    => (isset($readConfig['driver'])) ? $readConfig['driver'] : 'mysql' ,
+                    'host'      => (isset($readConfig['host'])) ? $readConfig['host'] : '127.0.0.1',
+                    'database'  => (isset($readConfig['database'])) ? $readConfig['database'] : 'test',
+                    'username'  => (isset($readConfig['user'])) ? $readConfig['user'] : 'root',
+                    'password'  => (isset($readConfig['password'])) ? $readConfig['password'] : 'root',
+                    'charset'   => 'utf8',
+                    'collation' => 'utf8_unicode_ci',
+                    'prefix'    => '',
+                    'strict'    => true,
+                    'modes'  => [
+                        //'ONLY_FULL_GROUP_BY',
+                        'STRICT_TRANS_TABLES',
+                        'NO_ZERO_IN_DATE',
+                        'NO_ZERO_DATE',
+                        'ERROR_FOR_DIVISION_BY_ZERO',
+                        'NO_ENGINE_SUBSTITUTION',
+                    ]
+                ],
+                'write' => [
+                    'driver'    => (isset($writeConfig['driver'])) ? $writeConfig['driver'] : 'mysql' ,
+                    'host'      => (isset($writeConfig['host'])) ? $writeConfig['host'] : '127.0.0.1',
+                    'database'  => (isset($writeConfig['database'])) ? $writeConfig['database'] : 'test',
+                    'username'  => (isset($writeConfig['user'])) ? $writeConfig['user'] : 'root',
+                    'password'  => (isset($writeConfig['password'])) ? $writeConfig['password'] : 'root',
+                    'charset'   => 'utf8',
+                    'collation' => 'utf8_unicode_ci',
+                    'prefix'    => '',
+                    'strict'    => true,
+                    'modes'  => [
+                        //'ONLY_FULL_GROUP_BY',
+                        'STRICT_TRANS_TABLES',
+                        'NO_ZERO_IN_DATE',
+                        'NO_ZERO_DATE',
+                        'ERROR_FOR_DIVISION_BY_ZERO',
+                        'NO_ENGINE_SUBSTITUTION',
+                    ]
+                ]
+
+            ]);
+        }
+        else{
+
+            $this->capsule->addConnection([
+                'driver'    => (isset($configdb['driver'])) ? $configdb['driver'] : 'mysql' ,
+                'host'      => (isset($configdb['host'])) ? $configdb['host'] : '127.0.0.1',
+                'database'  => (isset($configdb['database'])) ? $configdb['database'] : 'test',
+                'username'  => (isset($configdb['user'])) ? $configdb['user'] : 'root',
+                'password'  => (isset($configdb['password'])) ? $configdb['password'] : 'root',
+                'charset'   => 'utf8',
+                'collation' => 'utf8_unicode_ci',
+                'prefix'    => '',
+                'strict'    => true,
+                'modes'  => [
+                    //'ONLY_FULL_GROUP_BY',
+                    'STRICT_TRANS_TABLES',
+                    'NO_ZERO_IN_DATE',
+                    'NO_ZERO_DATE',
+                    'ERROR_FOR_DIVISION_BY_ZERO',
+                    'NO_ENGINE_SUBSTITUTION',
+                ]
+            ]);
+        }
+
+
 
         // Make this Capsule instance available globally via static methods... (optional)
         $this->capsule->setAsGlobal();
--- src/store/Services/DatabaseConnection.php
+++ src/store/Services/DatabaseConnection.php
@@ -11,28 +11,11 @@ class DatabaseConnection
     {
         if(config('database')!==null){
 
-            if(app()->has('clusterMethod')){
-                $method = app()->get('clusterMethod');
-            }
-            else{
-                $method = httpMethod();
-            }
-
             if(environment() == 'local'){
                 return config('database.connections.local');
             }
             else{
-                $config = config('database.connections.'.gethostname());
-
-                if(isset($config['read']) && $method=='get'){
-                    return $config['read'];
-                }
-
-                if(isset($config['write']) && $method!=='get'){
-                    return $config['write'];
-                }
-
-                return $config;
+                return config('database.connections.'.gethostname());
             }
         }
 
